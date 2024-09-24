7b5cc714809843c23d8e243452969f796c63b3c0
anastasiia19c <156657920+anastasiia19c@users.noreply.github.com>
2024-09-24 10:13:50 +0200

    rajout des instances

--- demo/src/main/java/com/formes/BackApplication.java
+++ demo/src/main/java/com/formes/BackApplication.java
@@ -15,7 +15,9 @@ public class BackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackApplication.class, args);
-        TriForme collection = new TriForme();
+        TriForme triParForme = new TriForme();
+        TriForme triParCouleur = new TriForme();
+        TriForme triParTaille = new TriForme();
 
         Forme carre = new Carre(10, "rouge", 2, 2);  
         Forme cercle = new Cercle(7, "bleu", 3,7); 
@@ -24,14 +26,42 @@ public class BackApplication {
         Forme pentagone = new Pentagone(9, "bleu", 3, 0); 
         Forme hexagone = new Hexagone(4, "vert", 4,8); 
 
-        collection.ajouterForme(carre);
-        collection.ajouterForme(cercle);
-        collection.ajouterForme(triangle);
-        collection.ajouterForme(rectangle);
-        collection.ajouterForme(pentagone);
-        collection.ajouterForme(hexagone);
+        Forme carreBleu = new Carre(10, "bleu", 2, 2);  
+        Forme cercleRouge = new Cercle(7, "rouge", 3,7); 
+        Forme triangleJaune = new Triangle(3, 4, 5, 5, 9, "jaune", 4,9);
+        Forme rectangleVert = new Rectangle(5, 9, "vert", 1, 3); 
+        Forme pentagoneBleu = new Pentagone(9, "bleu", 3, 0); 
+        Forme hexagoneRouge = new Hexagone(4, "rouge", 4,8); 
 
-        System.out.println(collection.toString());
+        Forme carrePetit = new Carre(2, "bleu", 2, 2);  
+        Forme cercleMoyen = new Cercle(6, "rouge", 3,7); 
+        Forme triangleGrand = new Triangle(12, 4, 5, 5, 9, "jaune", 4,9);
+        Forme rectanglePetit = new Rectangle(2, 9, "vert", 1, 3); 
+        Forme pentagoneMoyen = new Pentagone(6, "bleu", 3, 0); 
+        Forme hexagoneGrand = new Hexagone(12, "rouge", 4,8); 
+
+        triParForme.ajouterForme(carre);
+        triParForme.ajouterForme(cercle);
+        triParForme.ajouterForme(triangle);
+        triParForme.ajouterForme(rectangle);
+        triParForme.ajouterForme(pentagone);
+        triParForme.ajouterForme(hexagone);
+
+        triParCouleur.ajouterForme(carreBleu);
+        triParCouleur.ajouterForme(cercleRouge);
+        triParCouleur.ajouterForme(triangleJaune);
+        triParCouleur.ajouterForme(rectangleVert);
+        triParCouleur.ajouterForme(pentagoneBleu);
+        triParCouleur.ajouterForme(hexagoneRouge);
+
+        triParTaille.ajouterForme(carrePetit);
+        triParTaille.ajouterForme(cercleMoyen);
+        triParTaille.ajouterForme(triangleGrand);
+        triParTaille.ajouterForme(rectanglePetit);
+        triParTaille.ajouterForme(pentagoneMoyen);
+        triParTaille.ajouterForme(hexagoneGrand);
+
+        System.out.println(triParForme.toString());
         
     }
 }
