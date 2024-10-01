1188cba8f2ae5a8c38632ef929e1473f3eb0dd2e
anastasiia19c <156657920+anastasiia19c@users.noreply.github.com>
2024-10-01 10:44:38 +0200

    adding Angular

--- front/triForme/src/app/app.component.spec.ts
+++ front/triForme/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'TriForme' title`, () => {
+  it(`should have the 'triForme' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TriForme');
+    expect(app.title).toEqual('triForme');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TriForme');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, triForme');
   });
 });
