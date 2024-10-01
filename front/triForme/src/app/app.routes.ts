129a08901612551a6f46ec08cd14114354752191
anastasiia19c <156657920+anastasiia19c@users.noreply.github.com>
2024-10-01 11:30:36 +0200

    delete Angular

--- front/triForme/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { AcceuilComponent } from './acceuil/acceuil.component';
-
-export const routes: Routes = [
-  { path: '', component: AcceuilComponent }, // Route par défaut
-];
